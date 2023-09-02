d6105ec802331008a88d2d8632381fdac2f43f16
jmojedap <jmojedap@gmail.com>
2023-09-02 15:35:19 -0500

    Dynamic base route

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,10 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base:'/observatorio2023/',
+  /*base:'/observatorio2023/',*/
+  base: process.env.NODE_ENV === 'production'
+    ? '/observatorio2023/'
+    : '/observatorio2023/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
